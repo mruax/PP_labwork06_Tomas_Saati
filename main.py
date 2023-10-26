ebde525c5ca64fa70ba3f48c5d76dcb9428fc597
mruax <matvey.chernyakov@yandex.ru>
2023-10-27 01:12:12 +0500

    added possibility to change criteria name

--- main.py
+++ main.py
@@ -22,38 +22,27 @@ class MainWindow(QMainWindow):
         self.ui.tableWidget.horizontalHeader().setSectionsMovable(False)
         self.ui.tableWidget.verticalHeader().setSectionsMovable(False)
 
-        self.ui.tableWidget.horizontalHeader().sectionDoubleClicked.connect(self.editColumnHeader)
-        self.ui.tableWidget.verticalHeader().sectionDoubleClicked.connect(self.editRowHeader)
+        self.ui.tableWidget.horizontalHeader().sectionDoubleClicked.connect(self.editHeader)
+        self.ui.tableWidget.verticalHeader().sectionDoubleClicked.connect(self.editHeader)
 
         self.setWindowTitle("Tomas Saati")
         self.ui.result_button.clicked.connect(self.result)
 
         self.ui.criteriaAmount.valueChanged.connect(self.changeAmount)
+        self.amount = self.ui.criteriaAmount.value()
 
-    def editColumnHeader(self, logicalIndex):
-        item = self.ui.tableWidget.horizontalHeaderItem(logicalIndex)
-        n = self.ui.tableWidget.columnCount()
-        if logicalIndex == n - 1:
-            return
-        if item:
-            oldHeaders = [self.ui.tableWidget.horizontalHeaderItem(i).text() for i in range(n)]
-            newHeader, ok = QInputDialog.getText(self, 'Изменение данных', 'Название критерия:')
-            if ok:
-                oldHeaders[logicalIndex] = newHeader
-                self.ui.tableWidget.setHorizontalHeaderLabels(oldHeaders)
-
-    def editRowHeader(self, logicalIndex):
-        item = self.ui.tableWidget.verticalHeaderItem(logicalIndex)
+    def editHeader(self, logicalIndex):
         n = self.ui.tableWidget.rowCount()
         if logicalIndex == n - 1:
             return
-        if item:
-            oldHeaders = [self.ui.tableWidget.verticalHeaderItem(i).text() for i in
-                          range(n)]
-            newHeader, ok = QInputDialog.getText(self, 'Изменение данных', 'Название критерия:')
-            if ok:
-                oldHeaders[logicalIndex] = newHeader
-                self.ui.tableWidget.setVerticalHeaderLabels(oldHeaders)
+        oldHeaders = [self.ui.tableWidget.verticalHeaderItem(i).text() for i in range(n)]
+        oldHeaders2 = [self.ui.tableWidget.horizontalHeaderItem(i).text() for i in range(n)]
+        newHeader, ok = QInputDialog.getText(self, 'Изменение данных', 'Название критерия:')
+        if ok:
+            oldHeaders[logicalIndex] = newHeader
+            oldHeaders2[logicalIndex] = newHeader
+            self.ui.tableWidget.setVerticalHeaderLabels(oldHeaders)
+            self.ui.tableWidget.setHorizontalHeaderLabels(oldHeaders2)
 
     def result(self):
         print("Нажал кнопку!")
@@ -65,17 +54,39 @@ class MainWindow(QMainWindow):
                 if item is not None:
                     item.setText("")
 
+    def change_headers(self):
+        n = self.ui.tableWidget.columnCount()
+        headers = [self.ui.tableWidget.verticalHeaderItem(i).text() for i in range(n)]
+        headers[n - 2] = f"Критерий {n - 1}"
+        headers[n - 1] = "Сумма"
+        n = self.ui.tableWidget.rowCount()
+        self.ui.tableWidget.setVerticalHeaderLabels(headers)
+        headers = [self.ui.tableWidget.horizontalHeaderItem(i).text() for i in range(n)]
+        headers[n - 2] = f"Критерий {n - 1}"
+        headers[n - 1] = "Сумма"
+        self.ui.tableWidget.setHorizontalHeaderLabels(headers)
+
     def changeAmount(self):
-        # self.ui.tableWidget.setColumnCount(self.ui.tableWidget.columnCount() + 1)
-        # self.ui.tableWidget.setRowCount(self.ui.tableWidget.rowCount() + 1)
-        # row_position = self.ui.tableWidget.rowCount() - 1
-        # column_position = self.ui.tableWidget.columnCount() - 1
-        # self.ui.tableWidget.insertRow(row_position)
-        # self.ui.tableWidget.insertColumn(column_position)
         n = self.ui.criteriaAmount.value()
-        self.ui.tableWidget.setRowCount(n + 1)
-        self.ui.tableWidget.setColumnCount(n + 1)
+        if self.amount < n:
+            self.ui.tableWidget.insertRow(n)
+            self.ui.tableWidget.insertColumn(n)
+
+            row_header_item = QTableWidgetItem("Критерий " + str(n))
+            self.ui.tableWidget.setVerticalHeaderItem(n, row_header_item)
+            col_header_item = QTableWidgetItem("Критерий " + str(n))
+            self.ui.tableWidget.setHorizontalHeaderItem(n, col_header_item)
+
+            self.ui.tableWidget.setColumnCount(n + 1)
+            self.ui.tableWidget.setRowCount(n + 1)
+        else:
+            self.ui.tableWidget.removeRow(n)
+            self.ui.tableWidget.removeColumn(n)
+            self.ui.tableWidget.setColumnCount(n + 1)
+            self.ui.tableWidget.setRowCount(n + 1)
         self.nulify_cells()
+        self.amount = self.ui.criteriaAmount.value()
+        self.change_headers()
 
 
 if __name__ == "__main__":
